45554d18929cb421c7560bfc9ca2b1e525becfc6
Grzegorz Gackowski <greggupl1@gmail.com>
2020-03-20 19:28:16 +0100

    sth works

--- src/pollution.erl
+++ src/pollution.erl
@@ -10,7 +10,7 @@
 -author("grzegorz").
 
 %% API
--export([createMonitor/0]).
+-export([createMonitor/0, addStation/3, addValue/5]).
 
 % informacje o stacjach pogodowych
 % wspolrzedne geograficzne
@@ -20,5 +20,26 @@
 
 createMonitor() -> [[], []].
 
+addStation(StationName, {X, Y}, [Stations, Values]) ->
+  case lists:member(StationName, [Name || {Name, _} <- Stations]) of
+    true -> error;
+    false ->
+      case lists:member({X, Y}, [A || {_, A} <-  Stations]) of
+        true -> error;
+        false -> [[{StationName, {X, Y}} | Stations], Values]
+      end
+  end.
 
+addValue(Name, {{Year, Month, Day}, {Hour, Minute, Sec}}, Type, Value, [Stations, Values]) ->
+  case lists:member({Name, {{Year, Month, Day}, Hour, Minute, Sec}, Type},
+                    [{N, Tm, Tp} || {N, Tm, Tp, _} <- Values]) of
+    true -> error;
+    false ->
+      case lists:member(Name, [N || {N, _} <- Stations]) of
+        true -> [Stations, [{Name, {{Year, Month, Day}, {Hour, Minute, Sec}}, Type, Value} | Values]];
+        false -> error
+      end
+  end.
 
+% stations [{"name", {x, y}}, {}, {} ]
+% Values [{Name, Time, Type, Value}]
