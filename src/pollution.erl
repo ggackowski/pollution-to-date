c71d8c386afa48da480f8d9952ce4c3e3ef050b7
Grzegorz Gackowski <greggupl1@gmail.com>
2020-04-01 11:24:32 +0200

    sth

--- src/pollution.erl
+++ src/pollution.erl
@@ -10,7 +10,7 @@
 -author("grzegorz").
 
 %% API
--export([createMonitor/0, addStation/3, addValue/5]).
+-export([createMonitor/0, addStation/3, addValue/5, getStationByName/2]).
 
 % informacje o stacjach pogodowych
 % wspolrzedne geograficzne
@@ -33,13 +33,31 @@ addStation(StationName, {X, Y}, [Stations, Values]) ->
 addValue(Name, {{Year, Month, Day}, {Hour, Minute, Sec}}, Type, Value, [Stations, Values]) ->
   case lists:member({Name, {{Year, Month, Day}, Hour, Minute, Sec}, Type},
                     [{N, Tm, Tp} || {N, Tm, Tp, _} <- Values]) of
-    true -> error;
+    true -> error_repeat;
     false ->
       case lists:member(Name, [N || {N, _} <- Stations]) of
         true -> [Stations, [{Name, {{Year, Month, Day}, {Hour, Minute, Sec}}, Type, Value} | Values]];
-        false -> error
+        false -> error_nostation
       end
+  end;
+addValue({X, Y}, {{Year, Month, Day}, {Hour, Minute, Sec}}, Type, Value, [Stations, Values]) ->
+  Name = getStationByName(X, Y),
+  case Name of
+    [] -> error_noname;
+    _ -> addValue(Name, {{Year, Month, Day}, {Hour, Minute, Sec}}, Type, Value, [Stations, Values])
   end.
 
+getStationByName({X, Y}, [Stations, _]) ->
+  Res = lists:filter(fun({_, {Xx, Yy}}) -> (Xx == X) and (Yy == Y) end,  Stations),
+  case Res of
+    [] -> error;
+    _ -> Res
+  end.
+
+
+
+
+
+% removeValue(Name, )
 % stations [{"name", {x, y}}, {}, {} ]
 % Values [{Name, Time, Type, Value}]
